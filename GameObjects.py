2dfc1d5e924a41934cd5ffade887e3fe44d67948
seba2390 <madsen3008@gmail.com>
2022-12-14 00:43:38 +0100

    update

--- GameObjects.py
+++ GameObjects.py
@@ -1,5 +1,6 @@
 import pygame
 import numpy as np
+import random
 
 MULTIPLIER_TYPES = ["DLS",  # Double letter score
                     "TLS",  # Triple letter score
@@ -8,12 +9,28 @@ MULTIPLIER_TYPES = ["DLS",  # Double letter score
 
 CELL_TYPES = MULTIPLIER_TYPES + ["STANDARD"]
 
+ALPHABET = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+            "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+            "W", "X", "Y", "Z"]
+
+# Using https://en.wikipedia.org/wiki/Scrabble_letter_distributions
+LETTER_DISTRIBUTION = {
+                       "E": 12, "A": 9, "I": 9, "O": 8, "N": 6, "R": 6, "T": 6, "L": 4, "S": 4, "U": 4,  # 1 point
+                       "D": 4, "G": 3,                                                                   # 2 points
+                       "B": 2, "C": 2, "M": 2, "P": 2,                                                   # 3 points
+                       "F": 2, "H": 2, "V": 2, "W": 2, "Y": 2,                                           # 4 points
+                       "K": 1,                                                                           # 5 points
+                       "J": 1, "X": 1,                                                                   # 8 points
+                       "Q": 1, "Z": 1,                                                                   # 10 points
+                       }
+
 
 class Cell:
     def __init__(self, width: int,
                  height: int,
                  cell_type: str = "STANDARD",
-                 color: tuple[int, int, int] = (136, 136, 136)  # Grey as standard
+                 color: tuple[int, int, int] = (136, 136, 136),  # Grey as standard
+                 edge_color: tuple[int, int, int] = (0, 0, 0)
                  ) -> None:
         self.rect = None
 
@@ -22,6 +39,7 @@ class Cell:
         assert width == height, f'Width and height of cell should be equal, but they are: {self.width, self.height}'
 
         self.color = color
+        self.edge_color = edge_color
         self.type = cell_type
         assert cell_type in CELL_TYPES + ["STANDARD"], f'Type: {self.type} is not known, use any of: {CELL_TYPES}.'
 
@@ -50,7 +68,7 @@ class Cell:
 class Board:
     def __init__(self, nr_rows: int = 15,
                  nr_cols: int = 15,
-                 board_size: tuple[int, int] = (600,600)) -> None:
+                 board_size: tuple[int, int] = (600, 600)) -> None:
 
         self.nr_rows = nr_rows
         self.nr_cols = nr_cols
@@ -71,11 +89,30 @@ class Board:
             for _col in range(0, self.nr_cols):
                 _cell = Cell(width=self.cell_width, height=self.cell_height)
                 _cell.rect.left = _col * self.cell_width
-                _cell.rect.top  = _row * self.cell_height
+                _cell.rect.top = _row * self.cell_height
                 self.grid[_row][_col] = _cell
 
 
+class Letters:
+    def __init__(self, distribution=None):
+        if distribution is None:
+            distribution = LETTER_DISTRIBUTION
+        self._distribution = distribution
+        self.available_letters = []
 
+        self._initialize()
 
+    def _initialize(self):
+        for _letter in ALPHABET:
+            for _nr_letters in range(self._distribution[_letter]):
+                self.available_letters.append(_letter)
+        random.shuffle(self.available_letters)
 
+    def sample_hand(self, size: int = 7) -> list[int, ...]:
+        assert size <= len(self.available_letters), f'Not enough letters remaining, wanted size: {size}, \
+                                                     remaining letters: {len(self.available_letters)}.'
+        random.shuffle(self.available_letters)
+        _hand = self.available_letters[:size]
+        self.available_letters = self.available_letters[size:]
+        return _hand
 
--- ScrabbleEnvironment.py
+++ ScrabbleEnvironment.py
@@ -1,6 +1,5 @@
 from GameObjects import *
 
-
 class Scrabble:
     def __init__(self, seed: int, display_gameplay: bool):
 
@@ -8,7 +7,7 @@ class Scrabble:
         np.random.seed(self.seed)
         self.display_gameplay = display_gameplay
 
-        self.screen_color = (0, 0, 0)  # white in RGB
+        self.screen_color = (255, 255, 255)  # white in RGB
         self.display_gameplay = display_gameplay
         self.screen_size = self.screen_width, self.screen_height = 600, 800
         self.grid_size = self.rows, self.columns = 15, 15
@@ -33,22 +32,31 @@ class Scrabble:
         # Rendering grid
         for _row in range(self.rows):
             for _col in range(self.columns):
+                # Drawing cell fill
                 pygame.draw.rect(surface=self.window_surface,
                                  color=self.board.grid[_row][_col].color,
                                  rect=self.board.grid[_row][_col].rect,
-                                 width=2)
+                                 width=0)
+                # Drawing cell edge
+                pygame.draw.rect(surface=self.window_surface,
+                                 color=self.board.grid[_row][_col].edge_color,
+                                 rect=self.board.grid[_row][_col].rect,
+                                 width=1)
+
+        # Updating screen and forcing specific framerate
         pygame.display.update()
         self.clock.tick(self.fps)
 
+    # For handling user inputs
     def _handle_input(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.is_running = False
-                #raise SystemExit
 
     def get_state(self):
         pass
 
+    # For running game
     def run(self):
         self.is_running = True
         while self.is_running:
