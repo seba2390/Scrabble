1b71b8511778f40012169b26c9b87ea3b0ff7eec
seba2390 <madsen3008@gmail.com>
2022-12-25 16:59:32 +0100

    Added UK dictionary and improved submit functionality in Play class

--- Settings.py
+++ Settings.py
@@ -48,27 +48,29 @@ ALPHABET = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
 
 # Using https://en.wikipedia.org/wiki/Scrabble_letter_distributions
 LETTER_DISTRIBUTION = {
-                       " ": 2,                                                                           # 0 points
-                       "E": 12, "A": 9, "I": 9, "O": 8, "N": 6, "R": 6, "T": 6, "L": 4, "S": 4, "U": 4,  # 1 point
-                       "D": 4, "G": 3,                                                                   # 2 points
-                       "B": 2, "C": 2, "M": 2, "P": 2,                                                   # 3 points
-                       "F": 2, "H": 2, "V": 2, "W": 2, "Y": 2,                                           # 4 points
-                       "K": 1,                                                                           # 5 points
-                       "J": 1, "X": 1,                                                                   # 8 points
-                       "Q": 1, "Z": 1,                                                                   # 10 points
-                       }
+    " ": 2,  # 0 points
+    "E": 12, "A": 9, "I": 9, "O": 8, "N": 6, "R": 6, "T": 6, "L": 4, "S": 4, "U": 4,  # 1 point
+    "D": 4, "G": 3,  # 2 points
+    "B": 2, "C": 2, "M": 2, "P": 2,  # 3 points
+    "F": 2, "H": 2, "V": 2, "W": 2, "Y": 2,  # 4 points
+    "K": 1,  # 5 points
+    "J": 1, "X": 1,  # 8 points
+    "Q": 1, "Z": 1,  # 10 points
+}
 
 POINT_DISTRIBUTION = {
-                      " ": 0,
-                      "E": 1, "A": 1, "I": 1, "O": 1, "N": 1, "R": 1, "T": 1, "L": 1, "S": 1, "U": 1,
-                      "D": 2, "G": 2,
-                      "B": 3, "C": 3, "M": 3, "P": 3,
-                      "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4,
-                      "K": 5,
-                      "J": 8, "X": 8,
-                      "Q": 10, "Z": 10,
-                    }
+    " ": 0,
+    "E": 1, "A": 1, "I": 1, "O": 1, "N": 1, "R": 1, "T": 1, "L": 1, "S": 1, "U": 1,
+    "D": 2, "G": 2,
+    "B": 3, "C": 3, "M": 3, "P": 3,
+    "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4,
+    "K": 5,
+    "J": 8, "X": 8,
+    "Q": 10, "Z": 10,
+}
 
 WHITE = (255, 255, 255)
 GREY = (120, 120, 120)
 BLACK = (0, 0, 0)
+
+
